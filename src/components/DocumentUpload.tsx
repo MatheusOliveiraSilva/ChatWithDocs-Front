c85672370ccded52628a28eacbfc1fd9251e57f1
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-25 09:00:29 -0300

    fix: better positioning on document upload buttom

--- src/components/DocumentUpload.tsx
+++ src/components/DocumentUpload.tsx
@@ -242,9 +242,7 @@ const DocumentUpload: React.FC<DocumentUploadProps> = ({ onDocumentUploaded }) =
           strokeLinecap="round"
           strokeLinejoin="round"
         >
-          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-          <polyline points="17 8 12 3 7 8"></polyline>
-          <line x1="12" y1="3" x2="12" y2="15"></line>
+          <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.48-8.48l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"></path>
         </svg>
       </button>
 
